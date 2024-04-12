981e35be5e29d25de59547c6287f980406121326
irzko <tranminhkha.1003@gmail.com>
2024-04-12 14:03:46 -0500

    Remove unused code and dependencies

--- context/FilmContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export type FilmContextType = [films: Film[], setFilms: (film: Film[]) => void];
-
-const FilmContext = createContext<FilmContextType>([[], () => {}]);
-
-export default FilmContext;
